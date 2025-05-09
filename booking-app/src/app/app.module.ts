5ec7f43cf6c0dd847af165f2d9703de558647154
criz <96567084+2-3-32@users.noreply.github.com>
2025-05-09 10:52:49 +0000

    firebase integreate

--- booking-app/src/app/app.module.ts
+++ booking-app/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,3 +17,23 @@ import { AppRoutingModule } from './app-routing.module';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGvAVXZC-1uJ8Q-AyLk2-johJPCOKR9ZI",
+  authDomain: "login-d42df.firebaseapp.com",
+  databaseURL: "https://login-d42df-default-rtdb.firebaseio.com",
+  projectId: "login-d42df",
+  storageBucket: "login-d42df.firebasestorage.app",
+  messagingSenderId: "776717315675",
+  appId: "1:776717315675:web:3e6b27ba03fb0e377e197e",
+  measurementId: "G-K9RYNVGH9M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
